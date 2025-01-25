7282ae6f1e7799fb695000bb6baf5124bbe98168
Joseph Kelly <jsph.kelly@outlook.com>
2025-01-25 18:48:27 +0000

    Changes flag generation for testing

--- tests/test_flag_banner.py
+++ tests/test_flag_banner.py
@@ -1,24 +1,23 @@
 """Tests for the flag_banner module"""
 
-from terminal_flag_banner.flag_banner import TextBanner, generate_flag_list
+from terminal_flag_banner.flag_banner import TextBanner, generate_flag
+
+japan_flag = generate_flag("JP")
 
 
 def test_find_flag_column_length():
     """Test that the number of columns to display the flag in is calculated correctly"""
-    flag_list = generate_flag_list()
-    length = TextBanner._find_flag_column_length(flag=flag_list[0], needed_height=3)
+    length = TextBanner._find_flag_column_length(flag=japan_flag, needed_height=3)
     assert length == 10
 
 
 def test_calculate_flag_height():
     """Test that the height of the flag is calculated correctly"""
-    flag_list = generate_flag_list()
-    height = TextBanner._calculate_flag_height(flag=flag_list[0], columns=10)
+    height = TextBanner._calculate_flag_height(flag=japan_flag, columns=10)
     assert height == 3
 
 
 def test_find_banner_height():
     """Test that the height of the banner is calculated correctly"""
-    flag_list = generate_flag_list()
-    height = TextBanner._find_banner_height(flag=flag_list[0])
+    height = TextBanner._find_banner_height(flag=japan_flag)
     assert height == 13
